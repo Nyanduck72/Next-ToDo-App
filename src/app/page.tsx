44f3e58336d39f3bdcbd3af372f8e8bb099077fa
Alex <106273339+Nyanduck72@users.noreply.github.com>
2024-09-08 22:38:35 -0600

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,50 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="h-screen">
+        <div className="container mx-auto border-b-2 border-emerald-300/50 pt-4 pb-2">
+          <h1 className="text-emerald-400 text-center font-bold text-5xl lg:text-7xl md:text-6xl sm:text-5xl">
+            TO-DO APP
+          </h1>
+          <h5 className="my-4 text-emerald-900 text-center font-bold text-3xl lg:text-4xl">
+            but on Next.js
+          </h5>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="container grid grid-cols-12 gap-4 mx-auto my-12">
+          <div className="col-span-12 h-32 p-4 lg:col-span-4">
+            <h2 className="text-emerald-400 text-2xl text-center">ADD TASKS</h2>
+            <div className="grid grid-cols-12 gap-2 align-middle">
+              <div className="col-span-9 mt-4 flex justify-center">
+                <input
+                  type="text"
+                  name="task"
+                  id="task"
+                  className="text-slate-50 border-b-slate-400 border-b-2 bg-transparent h-full w-full p-2 focus:outline-0 focus:border-b-emerald-400"
+                />
+              </div>
+              <div className="col-span-3 mt-4 flex justify-center">
+                <div>
+                  <button className="bg-emerald-400 text-center text-xl text-slate-50 px-3 rounded-full h-full w-full hover:bg-emerald-500 active:bg-emerald-200 active:text-slate-800">
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-12 bg-slate-900 h-96 p-4 rounded-3xl shadow-lg lg:col-span-4">
+            <h2 className="text-emerald-400 text-2xl text-center">
+              CURRENT TASKS
+            </h2>
+            <div className=""></div>
+          </div>
+          <div className="col-span-12 bg-slate-900 h-96 p-4 rounded-3xl shadow-lg lg:col-span-4">
+            <h2 className="text-emerald-400 text-2xl text-center">
+              DONE TASKS
+            </h2>
+            <div className=""></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
